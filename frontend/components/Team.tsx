2af6fdb457928bd4f2da4a13bde6edba75aba635
Abbas Khan <78753275+Abbas-Khann@users.noreply.github.com>
2023-01-26 00:18:53 +0800

    Team Section completed

--- frontend/components/Team.tsx
+++ frontend/components/Team.tsx
@@ -1,14 +1,19 @@
 import React from "react";
 import teamImg from "../public/img/Team.png";
 import Image from "next/image";
+import teamOne from "../public/img/Team1.png";
 
 const Team = (): JSX.Element => {
     return(
         <section className="bg-black">
             <h1 className="text-4xl text-white text-center md:text-5xl pt-10 pb-24">Our Team</h1>
             <div className="flex items-center justify-evenly flex-wrap pb-40">
-            <Image src={teamImg} width={200} height={180} alt="TeamImg" />
-            <Image src={teamImg} width={200} height={180} alt="TeamImg" />
+            <div className="cursor-pointer">
+            <Image src={teamImg} width={350} height={180} alt="TeamImg" />
+            </div>
+            <div className="cursor-pointer">
+            <Image src={teamOne} width={350} height={180} alt="TeamImg" />
+            </div>
             </div>
         </section>
     )
