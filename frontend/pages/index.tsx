54aca1c13b0788c8e81d438152105cf4a201f41f
Abbas Khan <78753275+Abbas-Khann@users.noreply.github.com>
2023-01-25 20:05:27 +0800

    Created Team component

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -4,6 +4,7 @@ import Navbar from '@/components/Navbar';
 import Hero from '@/components/Hero';
 import Testimonial from '@/components/Testimonial';
 import Footer from '@/components/Footer';
+import Team from '@/components/Team';
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
@@ -19,6 +20,7 @@ export default function Home() {
         <Navbar />
         <Hero />
         <Testimonial />
+        <Team />
         <Footer />
       </main>
     </>
