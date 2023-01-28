7dbec8253564b4772a274d476ab90b89450b28bb
Dhruv-2003 <dhruvagarwal2017@gmail.com>
2023-01-28 23:49:18 +0530

    contract compile and deploy added

---
+++ frontend/functionality/deployContract.js
@@ -0,0 +1,61 @@
+const address = "";
+const sourceCode = "";
+
+async function compile(code) {
+  var input = {
+    language: "Solidity",
+    sources: {
+      "file.sol": {
+        content: code,
+      },
+    },
+    settings: {
+      outputSelection: {
+        "*": {
+          "*": ["*"],
+        },
+      },
+    },
+  };
+
+  var output = JSON.parse(solc.compile(JSON.stringify(input)));
+
+  /// getting out the contract file.sol
+  for (var contractName in output.contracts["file.sol"]) {
+    // preparing the response
+    var response = {
+      abi: output.contracts["file.sol"][contractName].abi, /// get the abi
+      bytecode: `0x${output.contracts["file.sol"][contractName].evm.bytecode.object}`, // get the bytecode
+    };
+    return response;
+  }
+}
+
+async function deploy(bytecode) {
+  ethereum
+    .request({
+      method: "eth_sendTransaction",
+      params: [
+        {
+          from: address,
+          gas: "0x2DC6C0",
+          data: bytecode,
+        },
+      ],
+    })
+    .then((data) => {
+      console.log(data);
+      return data;
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
+
+export async function deployContract() {
+  const data = await compile(sourceCode);
+  console.log(data);
+
+  const txData = await deploy(data.bytecode);
+  console.log(txData);
+}
