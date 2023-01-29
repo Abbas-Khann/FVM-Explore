341a27e3f2231759eb16178b5124567804dd543d
Abbas Khan <78753275+Abbas-Khann@users.noreply.github.com>
2023-01-29 19:04:23 +0800

    Added function

---
+++ frontend/components/ReturnedFunction.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+const ReadFunction = () => {
+    return(
+        <div
+        className="flex flex-col w-[318px] pb-5 bg-gray-300 px-8 border-t-8 border-orange-400 rounded-t-lg my-10 mx-10"
+        >
+            <h4 className="text-black text-xl py-2">Auction Drop Interval </h4>
+            <input
+            placeholder="address"
+            className="px-1 py-1 mb-2"
+            />
+            <input
+            placeholder="uint"
+            className="px-1 py-1 mb-2"
+            />
+            <div className="flex items-center justify-center">
+            <button
+            className="text-white text-lg bg-[#C41CFFC2] px-10 py-2 rounded-sm"
+            >
+            Submit
+            </button>
+            </div>
+            {/* returned value */}
+            <div>
+            <h2 className="break-all">
+            0x7B4A8d0862F049E35078E49F2561630Fac079eB9
+            </h2>
+            <h2>123</h2>
+            </div>
+        </div>
+    )
+}
+
+export default ReadFunction
--- frontend/pages/Explorer.tsx
+++ frontend/pages/Explorer.tsx
@@ -2,22 +2,32 @@ import React from "react";
 import Navbar from "@/components/Navbar";
 import Input from "@/components/Input";
 import Footer from "@/components/Footer";
+import ReadFunction from "@/components/ReturnedFunction";
 
 const Explorer = () => {
     return(
         <main className="bg-black min-h-[100vh]">
             <Navbar />
             <h1
-            className="bg-black text-white text-center text-3xl sm:text-4xl py-10">
-            Try Now
+            className="bg-black text-white text-center text-3xl sm:text-4xl pt-10 pb-14">
+            Explore here
             </h1>
-            <div className="mx-auto max-w-xl mb-10 px-4 ">
+            <div className="mx-auto max-w-xl mb-10 px-4">
                 <p className="pb-3 text-white text-xl">Paste Contract Address here</p>
                 <Input />
                 <div className="flex justify-evenly py-10">
                 <button className="text-white text-lg">Read Contract</button>
                 <button className="text-white text-lg">Write Contract</button>
                 </div>
+            </div>
+            <div
+            className="flex items-center justify-evenly flex-wrap"
+            >
+                <ReadFunction />
+                <ReadFunction />
+                <ReadFunction />
+                <ReadFunction />
+                <ReadFunction />
             </div>
             <Footer />
         </main>
