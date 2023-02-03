c6f61491c9de1b94eaa5ffce0345cfbde27835fa
Abbas Khan <78753275+Abbas-Khann@users.noreply.github.com>
2023-02-03 11:08:32 +0800

    Redirection to home page on logo click

--- frontend/components/Navbar.tsx
+++ frontend/components/Navbar.tsx
@@ -2,11 +2,14 @@ import React from 'react';
 import Image from 'next/image';
 import { CustomButton } from './CustomButton';
 import logo from "../public/img/logo.png";
+import Link from 'next/link';
 
 const Navbar = (): JSX.Element => {
   return (
         <nav className="flex items-center justify-center px-2 sm:px-4 py-4 bg-black">
+        <Link href={"/"}>
         <Image src={logo} width={200} height={180} alt="logo" />
+        </Link>
         <div className='flex flex-auto justify-end items-center'>
         <CustomButton />
         </div>
