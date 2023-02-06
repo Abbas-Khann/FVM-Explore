206d9766450ec4fd2ae2809e6f427cc3396a9d96
Abbas Khan <78753275+Abbas-Khann@users.noreply.github.com>
2023-02-06 17:18:45 +0800

    Added source code section

--- frontend/components/ReturnedSourceCode.tsx
+++ frontend/components/ReturnedSourceCode.tsx
@@ -1,12 +1,17 @@
 import React from "react";
+import { Code } from "@chakra-ui/react";
 
 const ReturnedSourceCode = (props: any): JSX.Element => {
     const { sourceCode } = props
 
     return(
-        <div className="text-white">
+        <Code 
+        colorScheme={"gray"}
+        variant="subtle"
+        padding={10}
+        >
             {sourceCode}
-        </div>
+        </Code>
     )
 }
 
